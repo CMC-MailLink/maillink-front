7f80968d4ac0283a9cb6d29c54d58e27b94f9ed9
NaDongHyeon <skehdgus401@gmail.com>
2022-02-19 23:15:42 +0900

    fix: merge 위해 stack.js에 alarm 부분 수정

--- navigation/Stacks.js
+++ navigation/Stacks.js
@@ -19,7 +19,7 @@ const Stacks = () => (
       headerShown: false,
     }}>
     <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
-    <Stack.Screen name="Alarm" component={Alarm} />
+    <Stack.Screen name="Alarm" component={Alarm} options={{headerTitle: ''}} />
     <Stack.Screen
       name="MailSearch"
       component={MailSearch}
