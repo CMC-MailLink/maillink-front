7eba1ebcf552faaaccdf7e9b03a70af0452141e5
NaDongHyeon <skehdgus401@gmail.com>
2022-02-25 01:43:24 +0900

    feat: SelfAuth Header 추가

--- Screen/SignUp/SelfAuth.js
+++ Screen/SignUp/SelfAuth.js
@@ -1,12 +1,66 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+} from 'react-native';
+import BackMail2 from '../../assets/images/BackMail2.png';
+import SignUpStep1 from '../../assets/images/SignUpStep1.png';
+import {useNavigation} from '@react-navigation/native';
 const SelfAuth = () => {
+  const navigation = useNavigation();
+
+  const onPressBack = () => {
+    navigation.goBack();
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>SelfAuth Screen</Text>
+    <View style={{flex: 1}}>
+      <SafeAreaView style={{flex: 0}} />
+      {/* upperHeader */}
+      <View style={styles.headerView}>
+        <TouchableWithoutFeedback onPress={onPressBack}>
+          <View style={{left: 24}}>
+            <Image style={{width: 9.5, height: 19}} source={BackMail2} />
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+      {/* mainHeader */}
+      <Image
+        style={{width: 39.05, height: 30.44, top: 25, left: 25}}
+        source={SignUpStep1}
+      />
+      <View style={{top: 20 + 15.22, left: 20}}>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.NameTitle}>본인인증</Text>
+          <Text style={styles.IntroTitle}>을</Text>
+        </View>
+        <Text style={styles.IntroTitle}>진행해주세요.</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerView: {
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingTop: 22,
+  },
+  NameTitle: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: 27,
+    color: '#3C3C3C',
+  },
+  IntroTitle: {
+    fontFamily: 'NotoSansKR-Light',
+    fontSize: 27,
+    color: '#3C3C3C',
+  },
+});
 
 export default SelfAuth;
