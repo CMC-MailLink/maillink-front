844062284be953fc6c08089c6a4dde15961170c4
yeon0914 <yeonoey0914@gmail.com>
2022-02-26 20:42:06 +0900

    feat:독자프로필 화면 구성

--- Screen/Author/Profile/AuthorProfile.js
+++ Screen/Author/Profile/AuthorProfile.js
@@ -1,12 +1,416 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+  TouchableWithoutFeedback,
+  Modal,
+} from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
+import SettingProfile from '../../../assets/images/SettingProfile.png';
+import DefaultProfile from '../../../assets/images/DefaultProfile.png';
+import ImageEditProfile from '../../../assets/images/ImageEditProfile.png';
+import CopyProfile from '../../../assets/images/CopyProfile.png';
+
+import AuthorProfileModal from './AuthorProfileModal';
 
 const AuthorProfile = () => {
+  const navigation = useNavigation();
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [name, setName] = useState('영이');
+  const [editName, setEditName] = useState('영이');
+  const [imageUri, setImageUri] = useState('');
+  const [introSelect, setIntroSelect] = useState(true);
+
+  const onPressModalConfirm = () => {
+    setName(editName);
+    setModalVisible(!modalVisible);
+  };
+  const onPressIntro = () => {
+    setIntroSelect(true);
+  };
+  const onPressMail = () => {
+    setIntroSelect(false);
+  };
+  const [filePath, setFilePath] = useState(null);
+  const [fileData, setFileData] = useState(null);
+  const [fileUri, setFileUri] = useState(null);
+
+  const copyToClipboard = data => {
+    Clipboard.setString(data);
+  };
+
+  const onPressEditImage = async () => {
+    const options = {
+      storageOptions: {
+        path: 'images',
+        mediaType: 'photo',
+        maxWidth: 78,
+        maxHeight: 78,
+      },
+      includeBase64: true,
+    };
+    launchImageLibrary(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.errorCode) {
+        console.log('ImagePicker Error: ', response.errorCode);
+        console.log('ImagePicker Error: ', response.errorMessage);
+      } else {
+        const source = {
+          uri: 'data:image/jpeg;base64,' + response.assets[0].base64,
+        };
+        setImageUri(source);
+      }
+    });
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>AuthorProfile Screen</Text>
+    <View style={{flex: 1}}>
+      <SafeAreaView style={{flex: 0, backgroundColor: '#4562F1'}} />
+      <StatusBar barStyle="light-content" />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <AuthorProfileModal
+          editName={editName}
+          setEditName={setEditName}
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+          onPressModalConfirm={onPressModalConfirm}></AuthorProfileModal>
+      </Modal>
+      <View style={styles.headerView}>
+        <Text style={styles.headerText}>프로필</Text>
+        <TouchableOpacity
+          style={{position: 'absolute', right: 20, bottom: 18}}
+          onPress={() => {
+            navigation.navigate('AuthorStacks', {
+              screen: 'Setting',
+            });
+          }}>
+          <Image
+            style={{
+              width: 18.68,
+              height: 19.2,
+            }}
+            source={SettingProfile}></Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.profileView}>
+        <View
+          style={{
+            alignItems: 'center',
+            top: -39,
+            width: 160,
+          }}>
+          <TouchableWithoutFeedback onPress={onPressEditImage}>
+            <Image
+              style={{width: 78, height: 78, borderRadius: 90}}
+              source={imageUri == '' ? DefaultProfile : imageUri}></Image>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={onPressEditImage}>
+            <Image
+              style={{width: 42, height: 42, top: -31, left: 25}}
+              source={ImageEditProfile}></Image>
+          </TouchableWithoutFeedback>
+          <View style={{alignItems: 'center', top: -37}}>
+            <Text style={styles.profileName}>{name}</Text>
+            <Text style={styles.profileCategory}>작가님</Text>
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+              <View style={styles.profileEditView}>
+                <Text style={styles.profileEditText}>프로필 수정</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bodyHeader}>
+        <View
+          style={{
+            width: 128,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <View style={introSelect ? styles.bodyHeaderBorder : null}>
+            <TouchableOpacity onPress={onPressIntro}>
+              <Text
+                style={{
+                  ...styles.bodyHeaderText,
+                  color: introSelect ? '#3C3C3C' : '#BEBEBE',
+                }}>
+                작가소개
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={introSelect ? null : styles.bodyHeaderBorder}>
+            <TouchableOpacity onPress={onPressMail}>
+              <Text
+                style={{
+                  ...styles.bodyHeaderText,
+                  color: introSelect ? '#BEBEBE' : '#000000',
+                }}>
+                작성메일
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bodyIntroView}>
+        <Text style={styles.bodyIntroHeadText}>소개</Text>
+        <Text style={styles.bodyIntroText}>
+          안녕하세요, 신진작가 ‘덩이’입니다 :) 재미있는 글을 쓰고 싶습니다.
+        </Text>
+      </View>
+      <View style={styles.bodyInterestView}>
+        <Text style={styles.bodyIntroHeadText}>관심사</Text>
+        <Text style={styles.bodyInterestHeadText}>갈래</Text>
+        <View style={{flexDirection: 'row', marginBottom: 10}}>
+          <TouchableOpacity>
+            <View style={styles.bodyInterestItemView1}>
+              <Text style={styles.bodyInterestItemText1}>시</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={styles.bodyInterestItemView2}>
+              <Text style={styles.bodyInterestItemText2}>소설</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={styles.bodyInterestItemView3}>
+              <Text style={styles.bodyInterestItemText3}>에세이</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.bodyInterestHeadText}>분위기</Text>
+        <View style={{flexDirection: 'row', marginBottom: 10}}>
+          <TouchableOpacity>
+            <View
+              style={{
+                ...styles.bodyInterestItemView2,
+                backgroundColor: '#E6DDFF',
+              }}>
+              <Text style={{...styles.bodyInterestItemText2, color: '#1E0072'}}>
+                서정
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View
+              style={{
+                ...styles.bodyInterestItemView2,
+                backgroundColor: '#C5F0E3',
+              }}>
+              <Text style={{...styles.bodyInterestItemText2, color: '#00402D'}}>
+                잔잔
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View
+              style={{
+                ...styles.bodyInterestItemView2,
+                backgroundColor: '#FFF2AD',
+              }}>
+              <Text style={{...styles.bodyInterestItemText2, color: '#3D3300'}}>
+                명랑
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View
+              style={{
+                ...styles.bodyInterestItemView2,
+                backgroundColor: '#FFDDDD',
+              }}>
+              <Text style={{...styles.bodyInterestItemText2, color: '#370000'}}>
+                유쾌
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View
+              style={{
+                ...styles.bodyInterestItemView2,
+                backgroundColor: '#FFE8FB',
+              }}>
+              <Text style={{...styles.bodyInterestItemText2, color: '#3E0035'}}>
+                달달
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={{
+          ...styles.bodyInterestView,
+          borderBottomWidth: 0,
+        }}>
+        <Text style={styles.bodyIntroHeadText}>관심사</Text>
+        <Text style={styles.bodyIntroText}>facebook.com/덩이</Text>
+        <TouchableOpacity
+          style={{position: 'absolute', top: 50, right: 20}}
+          onPress={() => copyToClipboard('facebook.com/덩이')}>
+          <Image style={{width: 13, height: 16}} source={CopyProfile}></Image>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerView: {
+    width: '100%',
+    height: 121 - 48,
+    backgroundColor: '#4562F1',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+  },
+  headerText: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: 16,
+    color: '#fff',
+  },
+  profileView: {
+    height: 150,
+    backgroundColor: '#fff',
+    borderBottomColor: '#F8F8F8',
+    borderBottomWidth: 3,
+    alignItems: 'center',
+  },
+  bodyHeader: {
+    height: 40,
+    borderBottomColor: '#EBEBEB',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    paddingLeft: 20,
+    alignItems: 'center',
+  },
+  bodyHeaderBorder: {
+    height: 40,
+    borderBottomWidth: 2,
+    borderBottomColor: '#4562F1',
+    justifyContent: 'center',
+  },
+  bodyHeaderText: {
+    fontFamily: 'NotoSansKR-Regular',
+    fontSize: 14,
+  },
+  profileName: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: 20,
+    color: '#3C3C3C',
+    includeFontPadding: false,
+  },
+  profileCategory: {
+    fontFamily: 'NotoSansKR-Regular',
+    fontSize: 16,
+    color: '#BEBEBE',
+    includeFontPadding: false,
+  },
+  profileEditView: {
+    width: 75,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    borderColor: '#BEBEBE',
+    borderWidth: 1,
+    marginTop: 7,
+  },
+  profileEditText: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: 12,
+    color: '#3C3C3C',
+  },
+  bodyIntroView: {
+    height: 114,
+    paddingTop: 19,
+    paddingBottom: 19,
+    paddingLeft: 21,
+    paddingRight: 21,
+    borderBottomColor: '#EBEBEB',
+    borderBottomWidth: 1,
+  },
+  bodyIntroHeadText: {
+    fontFamily: 'NotoSansKR-Medium',
+    fontSize: 14,
+    color: '#3C3C3C',
+    height: 30,
+  },
+  bodyIntroText: {
+    fontFamily: 'NotoSansKR-Light',
+    fontSize: 14,
+    color: '#828282',
+  },
+  bodyInterestView: {
+    paddingVertical: 19,
+    paddingHorizontal: 21,
+    borderBottomColor: '#EBEBEB',
+    borderBottomWidth: 1,
+  },
+  bodyInterestHeadText: {
+    fontFamily: 'NotoSansKR-Light',
+    fontSize: 12,
+    color: '#828282',
+    marginBottom: 5,
+  },
+  bodyInterestItemView1: {
+    width: 43,
+    height: 24,
+    borderRadius: 26,
+    backgroundColor: '#E8EBFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 7,
+  },
+  bodyInterestItemText1: {
+    fontFamily: 'NotoSansKR-Regular',
+    fontSize: 12,
+    color: '#0021C6',
+  },
+  bodyInterestItemView2: {
+    width: 53,
+    height: 24,
+    borderRadius: 26,
+    backgroundColor: '#E8EBFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 7,
+  },
+  bodyInterestItemText2: {
+    fontFamily: 'NotoSansKR-Regular',
+    fontSize: 12,
+    color: '#0021C6',
+  },
+  bodyInterestItemView3: {
+    width: 63,
+    height: 24,
+    borderRadius: 26,
+    backgroundColor: '#E8EBFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 7,
+  },
+  bodyInterestItemText3: {
+    fontFamily: 'NotoSansKR-Regular',
+    fontSize: 12,
+    color: '#0021C6',
+  },
+});
 
 export default AuthorProfile;
---
+++ Screen/Author/Profile/AuthorProfileSearch.js
@@ -0,0 +1,320 @@
+import React, {useEffect, useLayoutEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+
+import BackMail from '../../../assets/images/BackMail.png';
+import SearchMail from '../../../assets/images/SearchMail.png';
+import RecentSearchMail from '../../../assets/images/RecentSearchMail.png';
+import DeleteMail from '../../../assets/images/DeleteMail.png';
+import NoDataMail from '../../../assets/images/NoDataMail.png';
+import AuthorProfileImage from '../../../assets/images/AuthorProfileImage.png';
+
+const AuthorMailSearch = () => {
+  const [recentSearch, setRecentSearch] = useState([
+    '이작가',
+    '별 헤는 밤',
+    '파란 하늘',
+  ]);
+  const [mail, setMail] = useState([
+    {
+      key: '0',
+      author: '이작가',
+      title: '청춘예찬2',
+      body: '피가 광야에서 이는 위하여 없으면, 풍부 하게 심장의 영락과 곳으로 것이다. 끝',
+      date: '21. 02. 13',
+    },
+    {
+      key: '1',
+      author: '김작가',
+      title: '별 헤는 밤',
+      body: '하나에 경, 우는 이국 그리워 파란 애기듯 합니다.오는 잔디가 밤이 봅니다. 말같',
+      date: '21. 02. 12',
+    },
+    {
+      key: '2',
+      author: '이작가',
+      title: '청춘예찬',
+      body: '하나에 경, 우는 이국 그리워 파란 애기듯 합니다.오는 잔디가 밤이 봅니다. 말같',
+      date: '21. 02. 11',
+    },
+    {
+      key: '3',
+      author: '최작가',
+      title: '파란 하늘',
+      body: '피가 광야에서 이는 위하여 없으면, 풍부 하게 심장의 영락과 곳으로 것이다. 끝',
+      date: '21. 02. 10',
+    },
+  ]);
+  const [delIndex, setDelIndex] = useState(null);
+  const [query, setQuery] = useState('');
+  const [submit, setSubmit] = useState(false);
+  const [result, setResult] = useState([]);
+  const navigation = useNavigation();
+  const onPressBack = () => {
+    navigation.goBack();
+  };
+  const onPressRecentSearch = data => {
+    setQuery(data);
+    setSubmit(true);
+    var res = mail.filter(
+      item => item.author.includes(data) || item.title.includes(data),
+    );
+    setResult([...res]);
+  };
+  const onPressDelete = (data, index) => {
+    var temp = recentSearch;
+    temp.splice(index, 1);
+    setRecentSearch([...temp]);
+  };
+  const onChangeText = text => setQuery(text);
+  const onSubmit = () => {
+    if (query === '') {
+      return;
+    }
+    setSubmit(true);
+    var res = mail.filter(
+      item => item.author.includes(query) || item.title.includes(query),
+    );
+    setResult([...res]);
+  };
+  const onPressMailItem = data => {
+    navigation.navigate('AuthorStacks', {
+      screen: 'Reading',
+      params: {item: {...data}},
+    });
+  };
+
+  useEffect(() => {
+    if (query === '') setSubmit(false);
+  }, [query]);
+
+  return (
+    <View style={{flex: 1}}>
+      <View style={styles.headerView}>
+        <Text style={styles.headerText}>메일 검색</Text>
+        <View style={styles.headerSearchContainer}>
+          <TouchableWithoutFeedback onPress={onPressBack}>
+            <View>
+              <Image style={{width: 9.5, height: 19}} source={BackMail}></Image>
+            </View>
+          </TouchableWithoutFeedback>
+          <View>
+            <View style={styles.headerSearchBar}>
+              <TextInput
+                style={styles.searchTextInput}
+                value={query}
+                placeholder="작가 또는 제목을 검색해보세요."
+                placeholderTextColor="#D2D2D2"
+                returnKeyType="search"
+                onChangeText={onChangeText}
+                onSubmitEditing={onSubmit}></TextInput>
+              <TouchableWithoutFeedback onPress={onSubmit}>
+                <View style={styles.searchView}>
+                  <Image
+                    style={{
+                      width: 22,
+                      height: 22,
+                    }}
+                    source={SearchMail}
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </View>
+        </View>
+      </View>
+      <ScrollView>
+        {submit ? (
+          <View>
+            <View style={styles.titleView}>
+              <Text style={styles.titleText}>메일에서 찾은 결과</Text>
+            </View>
+            {result.length ? (
+              result.map((data, index) => (
+                <TouchableWithoutFeedback
+                  onPress={e => onPressMailItem(data)}
+                  key={index}>
+                  <View
+                    style={{
+                      height: 114,
+                      backgroundColor: '#FFF',
+                      paddingTop: 14,
+                      borderBottomColor: '#EBEBEB',
+                      borderBottomWidth: 1,
+                    }}>
+                    <Image
+                      style={{
+                        position: 'absolute',
+                        width: 42,
+                        height: 42,
+                        left: 36,
+                        top: 14,
+                      }}
+                      source={AuthorProfileImage}
+                    />
+                    <View style={{flexDirection: 'row'}}>
+                      <Text
+                        style={{
+                          color: '#4562F1',
+                          fontFamily: 'NotoSansKR-Bold',
+                          fontSize: 16,
+                          left: 93,
+                        }}>
+                        {data.author}
+                      </Text>
+                      <Text
+                        style={{
+                          position: 'absolute',
+                          color: '#BEBEBE',
+                          fontFamily: 'NotoSansKR-Thin',
+                          fontSize: 12,
+                          right: 20,
+                        }}>
+                        {data.date}
+                      </Text>
+                    </View>
+                    <Text
+                      style={{
+                        color: '#000',
+                        fontFamily: 'NotoSansKR-Bold',
+                        fontSize: 14,
+                        left: 93,
+                      }}>
+                      {data.title}
+                    </Text>
+                    <Text
+                      style={{
+                        color: '#828282',
+                        fontFamily: 'NotoSansKR-Thin',
+                        fontSize: 14,
+                        left: 93,
+                        width: 230,
+                      }}>
+                      {data.body}
+                    </Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              ))
+            ) : (
+              <Image
+                style={{width: 390, height: 78, top: 244}}
+                source={NoDataMail}></Image>
+            )}
+          </View>
+        ) : (
+          <View>
+            <View style={styles.titleView}>
+              <Text style={styles.titleText}>최근 검색</Text>
+            </View>
+            {recentSearch.map((data, index) => (
+              <TouchableOpacity
+                onPress={e => onPressRecentSearch(data)}
+                key={index}>
+                <View style={styles.recentSearch}>
+                  <Image
+                    style={{width: 35, height: 35, left: 23}}
+                    source={RecentSearchMail}></Image>
+                  <Text style={styles.recentSearchText}>{data}</Text>
+                  <TouchableWithoutFeedback
+                    onPress={e => onPressDelete(data, index)}>
+                    <Image
+                      style={{
+                        position: 'absolute',
+                        width: 12,
+                        height: 12,
+                        right: 28,
+                      }}
+                      source={DeleteMail}></Image>
+                  </TouchableWithoutFeedback>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+        )}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerView: {
+    width: '100%',
+    height: 150 - 48,
+    backgroundColor: '#4562F1',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  headerText: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: 16,
+    color: '#fff',
+  },
+  headerBack: {
+    height: 150 - 48,
+    width: 20,
+  },
+  headerSearchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  headerSearchBar: {
+    backgroundColor: '#fff',
+    width: 322,
+    height: 44,
+    borderRadius: 43,
+    marginLeft: 15,
+    justifyContent: 'center',
+    paddingLeft: 20,
+    paddingRight: 44,
+  },
+  searchView: {
+    position: 'absolute',
+    right: 0,
+    width: 44,
+    height: 44,
+    borderRadius: 90,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchTextInput: {
+    fontFamily: 'NotoSansKR-Regular',
+    fontSize: 15,
+  },
+  titleView: {
+    height: 44,
+    width: '100%',
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    paddingLeft: 21,
+  },
+  titleText: {
+    fontFamily: 'NotoSansKR-Medium',
+    fontSize: 14,
+    color: '#000',
+  },
+  recentSearch: {
+    height: 50,
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  recentSearchText: {
+    marginLeft: 40,
+    fontFamily: 'NotoSansKR-Regular',
+    fontSize: 16,
+  },
+});
+
+export default AuthorMailSearch;
