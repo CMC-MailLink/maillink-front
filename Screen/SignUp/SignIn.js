f42d9c8188b446ca3a4d48c980be5453e57e3ae5
NaDongHyeon <skehdgus401@gmail.com>
2022-03-05 02:18:40 +0900

    fix: signIn 경로 정상 설정

--- Screen/SignUp/SignIn.js
+++ Screen/SignUp/SignIn.js
@@ -17,6 +17,7 @@ import LineSignIn from '../../assets/images/LineSignIn.png';
 
 const SignIn = () => {
   const navigation = useNavigation();
+
   const onPressNaverLogin = () => {
     navigation.navigate('SignUpStacks', {
       screen: 'SetProfileSelfAuth',
@@ -25,7 +26,7 @@ const SignIn = () => {
 
   const getSetProfile = () => {
     navigation.navigate('SignUpStacks', {
-      screen: 'SelectUserType',
+      screen: 'SetProfile',
     });
   };
 
@@ -73,7 +74,7 @@ const SignIn = () => {
             justifyContent: 'space-between',
             alignItems: 'center',
           }}>
-          <TouchableOpacity onPress={getSetProfile}>
+          <TouchableOpacity onPress={signInWithKakao}>
             <Image style={{width: 312, height: 52}} source={KakaoLogin} />
           </TouchableOpacity>
           <TouchableOpacity onPress={getProfile}>
